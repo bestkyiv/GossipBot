539980d6b1bc6f978eedbb786dc29b0584b93937
Artem Taranych <artem.taranych@listat.com.ua>
2025-02-13 23:44:51 +0200

    *v2

--- src/gossip_bot.py
+++ src/gossip_bot.py
@@ -1,74 +1,139 @@
-from telegram import Update
+from telegram import Message, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
-from settings import Settings
+from env_config import Config
+import random
 
-# todo save to storage
-user_access = {}
-settings = Settings()
+# todo
+lbg_people = []
+
+gossip_text_starts = [
+	"Я тут таке почув 😮",
+	"Тут ходять чутки, що...",
+	"Інсайдік під'їхав 🕵️",
+	"Ти не повіриш, але",
+	"Кажуть, що...",
+	"Слухай, що я дізнався 🤯",
+]
+
+gossip_photo_starts = [
+	"Подивись на це 😲",
+	"Тут таке фото надіслали...",
+	"Ти бачив це? 😳",
+	"Це просто неймовірно!",
+	"Що скажеш на це? 📸",
+	"Ось що я знайшов 🤯",
+]
+
+config = Config()
 
 # Command handler for '/start'
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = update.effective_user.id
+async def start_async(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	if access_granted(update):
+		await update.message.reply_text("Вітаю! Надішліть повідомлення, і я перешлю його в чат.")
+	else:
+		await update.message.reply_text("На жаль, у вас немає доступу до цього бота.")
 
-    if settings.is_access_check_enable() and user_id not in user_access:
-        await update.message.reply_text("Welcome! Please enter the password to gain access:")
-    else:
-        await update.message.reply_text("You already have access. Send a message, and I'll forward it to the chat.")
+# Command handler for '/help'
+async def help_async(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	text = \
+	"""
+		Надішліть повідомлення мені, і я перешлю його у обраний чат.\n
+		Якщо у вас немає доступу, зверніться до @eltagun.
+	"""
+	await update.message.reply_text(text)
 
-# Command handler for '/chat_id'
-async def get_chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.effective_chat.id
-    name = update.effective_chat.title
+# Command handler for '/enable'
+async def enable_async(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	if not update.effective_chat.type == 'group' and not update.effective_chat.type == 'supergroup':
+		await update.message.reply_text("Ця команда доступна тільки в групових чатах.")
+		return
 
-    # todo catch exceptions
-    await update.message.reply_text(
-        f"Chat ID: `{chat_id}`.\nName: `{name}`",
-        parse_mode='Markdown')
+	if not access_granted(update):
+		await update.message.reply_text("На жаль, у вас немає доступу до цієї команди.")
+		return
 
-async def check_password(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not settings.is_access_check_enable():
-        return
-    # todo
-    # user_id = update.effective_user.id
-    # if user_id in user_access:
-    #     await update.message.reply_text("You already have access. Send a message, and I'll forward it to the chat.")
-    #     return  # User already has access
-
-    # if update.message.text == PASSWORD:
-    #     user_access[user_id] = True
-    #     await update.message.reply_text("Access granted! You can now send messages to be forwarded.")
-    # else:
-    #     await update.message.reply_text("Incorrect password. Try again.")
+	config.set_forvarding(config.is_forwarding_enable() is False)
+	await update.message.reply_text(f"Бота {'ввімкнено' if config.is_forwarding_enable() else 'вимкнено'}!")
 
 # Message handler for forwarding messages
-async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if settings.is_access_check_enable():
-        await check_password(update, context)
-        return
+async def forward_message_async(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    text = f"🗣️ <b>Gossip</b>\n<blockquote>{update.message.text}</blockquote>"
+	if not access_granted(update):
+		await update.message.reply_text("На жаль, ви не маєте доступу до написання госіпів 🥲")
+		return
+	
+	if update.effective_chat.type == 'group' or update.effective_chat.type == 'supergroup':
+		return
 
-    # todo catch exceptions
-    await context.bot.send_message(
-        chat_id=settings.get_chat_id(),
-        text=text,
-        parse_mode='HTML')
+	if config.is_forwarding_enable() is False:
+		await update.message.reply_text("Бота в групі було вимкнено!")
+		return
 
-    await update.message.reply_text(
-        text="The gossip has been sended!",
-        parse_mode='Markdown')
+	# todo catch exceptions
+	if update.message.text:
+		await forward_text_async(update.message.text, context)
+	elif update.message.media_group_id:
+		await forward_media_group_async(update, context)
+		return
+	elif update.message.photo:
+		await forward_photo_async(update.message, context)
+	else:
+		await update.message.reply_text("Невідомий тип повідомлення")
+		return
 
+	await update.message.reply_text("Повідомлення надіслано!")
+
+async def forward_text_async(text: str, context: ContextTypes.DEFAULT_TYPE):
+	start_msg = random.choice(gossip_text_starts)
+	text = f"🗣️ <b>{start_msg}</b>\n<blockquote>{text}</blockquote>"
+
+	await context.bot.send_message(
+		chat_id=config.get_chat_id(),
+		text=text,
+		parse_mode='HTML')
+
+async def forward_photo_async(message: Message, context: ContextTypes.DEFAULT_TYPE):
+	text = None
+	if message.caption is not None:
+		start_msg = random.choice(gossip_photo_starts)
+		text = f"🗣️ <b>{start_msg}</b>\n<blockquote>{message.caption}</blockquote>"
+
+	fileID = message.photo[-1]['file_id']
+	await context.bot.send_photo(
+		chat_id=config.get_chat_id(),
+		photo = fileID,
+		caption = text,
+		parse_mode='HTML')
+
+async def forward_media_group_async(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	await update.message.reply_text("Я не вмію в групові фото. Надішліть їх по одному з підписом.")
+	return
+
+def access_granted(update: Update) -> bool:
+	if not config.is_access_check_enable():
+		return True
+
+	user_tag = update.effective_user.username
+	if user_tag in config.get_coreteam_people():
+		return True
+
+	if user_tag in lbg_people:
+		return True
+	return False
 
 def main():
-    application = ApplicationBuilder().token(settings.get_access_token()).build()
+	application = ApplicationBuilder().token(config.get_access_token()).build()
 
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("chat_id", get_chat_id))
-    # todo handler for chat configuration
+	application.add_handler(CommandHandler("start", start_async))
+	application.add_handler(CommandHandler("help", help_async))
+	application.add_handler(CommandHandler("enable", enable_async))
 
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message))
+	# tbd /set_public_password
 
-    application.run_polling()
+	application.add_handler(MessageHandler((filters.TEXT | filters.PHOTO) & ~filters.COMMAND, forward_message_async))
+
+	application.run_polling()
+	print("Bot is running...")
 
 if __name__ == "__main__":
-    main()
+	main()
